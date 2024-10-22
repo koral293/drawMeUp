367e17b9cddf94dcf94fa0d5a1b85f3eec28da25
Tomek <tomekcabaj@gmail.com>
2024-10-22 21:24:03 +0200

    Post_item layout

--- app/src/main/java/com/example/drawmeup/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/drawmeup/ui/home/HomeFragment.kt
@@ -6,37 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.drawmeup.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        return FragmentHomeBinding.inflate(layoutInflater, container, false)
+            .also {
+                binding = it
+                it.viewModel = viewModel
+                binding.lifecycleOwner = this
+            }.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
