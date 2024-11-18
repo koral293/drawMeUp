d26f2b53d60fda05bea2873b5a04a2e2e4455558
Damian Karkowski <damian@mbp-damian.home>
2024-11-18 21:36:53 +0100

    Add pull to post fragment
    Signed-off-by: Damian Karkowski <damian@mbp-damian.home>

--- app/src/main/java/com/example/drawmeup/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/drawmeup/ui/post/PostFragment.kt
@@ -1,25 +1,33 @@
 package com.example.drawmeup.ui.post
 
+import android.os.Build
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.drawmeup.R
+import com.example.drawmeup.data.models.Post
+import com.example.drawmeup.databinding.FragmentPostBinding
+import com.example.drawmeup.navigation.PostType
+import com.example.drawmeup.utils.Logger
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.runBlocking
 
+private const val TYPE_KEY = "type"
 class PostFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PostFragment()
-    }
-
     private val viewModel: PostViewModel by viewModels()
+    private lateinit var binding: FragmentPostBinding
+    private lateinit var type: PostType
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            type = it.getSerializable(TYPE_KEY, PostType::class.java) ?: PostType.View(0)
+        }
         // TODO: Use the ViewModel
     }
 
@@ -28,8 +36,30 @@ class PostFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)?.visibility = View.GONE
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        return FragmentPostBinding.inflate(layoutInflater, container, false).also {
+            binding = it
+            it.viewModel = viewModel
+            it.lifecycleOwner = this
+        }.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(viewModel) {
+            (type as? PostType.View)?.let { init(it.id) }
+
+            runBlocking {
+                val post = postRepository.getPostById((type as? PostType.View)?.id ?: 0)
+                Logger.debug("Post found: $post")
+                viewModel.name.value = post?.name
+                viewModel.description.value = post?.description
+                viewModel.image.value = post?.postData
+                viewModel.tags.value = post?.tag?.joinToString { it }
+                viewModel.author.value = post?.userId.toString()
+                binding.imageView.setImageBitmap(post?.postData)
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
